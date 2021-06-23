16fb6523f6e2288879f675e6235fecf32a9a88bf
Tommi <tooj1800@student.miun.se>
2021-06-23 14:58:28 +0200

    index.php and merge-ish

--- public/index.php
+++ public/index.php
@@ -1,13 +1,82 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat.app</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-<input type="date" value="">
+
+<body class="wave-bg">
+    <header class="flex">
+        <a href="index.html">chat.app</a>
+
+        <nav>
+            <a href="about.html">about</a>
+            <a href="signup.html" class="button button-outline">sign up</a>
+            <a href="login.html" class="button">sign in</a>
+        </nav>
+
+    </header>
+
+    <main>
+
+        <div class="hero">
+            <h1>
+                Totally the best chat client in the world
+            </h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam fugiat tempora eos facere obcaecati,
+                dolorem suscipit? Sunt maiores commodi alias mollitia voluptates id quis cumque, modi eaque
+                accusamus sequi! Odio ad voluptates et sunt rem, repudiandae nulla eos officia eligendi accusantium
+                ea nemo dolorem quasi fugiat deserunt? A, quisquam quaerat.</p>
+
+                <a href="signup.html" class="button button-outline">get started!</a>
+        </div>
+        <div id="main-container">
+
+            <div id="app" style="display: none;">
+
+
+
+            </div>
+
+            <!-- <div id="send-message">Button</div> -->
+        </div>
+    </main>
+
+    <footer>
+        &copy; 2021 - Rights reserved osv<br>
+        <a href="mailto:info@example.com">info@chatapp.com</a>
+    </footer>
+
+    <!-- <div class="wave">
+        <img src="assets/img/wave.svg" alt="">
+    </div> -->
+
+    <script src="script/script.js" type="text/javascript"></script>
+
+    <script>
+        let socket = new Websocket("ws://localhost:5500");
+        let buttonElement = document.getElementById("send-message");
+
+        async function sendMessage(message) {
+            await socket.send(message);
+            let response = await socket.receive();
+            console.log(response);
+        }
+
+        buttonElement.addEventListener("click", async () => {
+            await sendMessage("Clicked the button");
+        });
+
+        window.addEventListener("load", async () => {
+            await socket.open();
+            await sendMessage("Opened the connection");
+        });
+    </script>
 </body>
+
 </html>
