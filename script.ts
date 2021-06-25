56ee8c9c64fa54e591a7094d802d0802936e8605
Tommi <tooj1800@student.miun.se>
2021-06-25 23:49:55 +0200

    Bascially nothing. Just removed some trash.

--- script.ts
+++ script.ts
@@ -7,5 +7,5 @@ const appEl = document.getElementById("app")!;
 const chosen: number = 6;
 for(let i = 0; i < 10; i++){
     let product: number = multiplication(chosen, i);
-    appEl.innerHTML += `<span>${chosen} x ${i} = ${product}</span><br>`;
+    // appEl.innerHTML += `<span>${chosen} x ${i} = ${product}</span><br>`;
 }
