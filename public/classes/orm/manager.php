4665f4663f8671b248d9e271fd678606fa58583d
Tommi <tooj1800@student.miun.se>
2021-06-25 22:08:09 +0200

    Class DbManager construct

--- public/classes/orm/manager.php
+++ public/classes/orm/manager.php
@@ -10,3 +10,20 @@ include_once __DIR__ . "/models/userProfile.php";
 // password: $_SERVER["DB_PASS"]
 // database name: $_SERVER["DB_NAME"]
 
+class DbManager
+{
+    private $dbConn;
+
+    function __construct()
+    {
+        $this->dbConn = new mysqli(
+            $_SERVER["DB_HOSTNAME"],
+            $_SERVER["DB_USER"],
+            $_SERVER["DB_PASS"],
+            $_SERVER["DB_NAME"]
+        );
+        if ($this->connection->connect_errno !== 0) {
+            die("Could not connect. Error: " . $this->connection->connect_errno . " " . $this->connection->connect_error);
+        }
+    }
+}
