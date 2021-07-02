4c9f58caa4e393228dc2c9c8f6f4811643d4bfe3
EliasEriksson <EliasEriksson95@gmail.com>
2021-07-02 23:04:50 +0200

    started work on FileField upload directory.

--- public/classes/forms/fields/fileField.php
+++ public/classes/forms/fields/fileField.php
@@ -5,6 +5,14 @@ include_once __DIR__ . "/labeledField.php";
 
 class FileField extends LabeledField
 {
+    private string $uploadDirectory;
+
+    public function __construct(string $labelText, string $name, string $value = "", bool $refillOnFailedPost = true, bool $mustValidate = true)
+    {
+        parent::__construct($labelText, $name, $value, $refillOnFailedPost, $mustValidate);
+
+    }
+
     public function toHTML(): string
     {
         $html = $this->wrapWithSpan($this->labelText);
