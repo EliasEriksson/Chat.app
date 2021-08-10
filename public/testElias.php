c800f5ee7be0c15415da2a57d2d1e90b4525647d
EliasEriksson <EliasEriksson95@gmail.com>
2021-08-10 11:15:41 +0200

    added head.php

--- public/testElias.php
+++ public/testElias.php
@@ -1,31 +1,12 @@
-<?php
-include_once __DIR__ . "/config.php";
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST["get-session"])) {
-        session_regenerate_id();
-    } else if (isset($_POST["destroy-session"])) {
-        session_destroy();
-    }
-    header("location: ./testElias.php");
-}
-var_dump($_SESSION);
-echo session_id() . "<br>";
-?>
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elias Test</title>
+    <?php include "$rootURL/includes/head.php" ?>
+    <title>testElias.php</title>
 </head>
 <body>
-<form method="post" enctype="application/x-www-form-urlencoded">
-    <input type="submit" name="get-session" value="get">
-</form>
-<form method="post" enctype="application/x-www-form-urlencoded">
-    <input type="submit" name="destroy-session" value="destroy">
-</form>
-<script type="module" src="/script/client/main.js"></script>
+<?php include "$rootURL/includes/header.php" ?>
+
+<?php include "$rootURL/includes/footer.php" ?>
 </body>
 </html>
